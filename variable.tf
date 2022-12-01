a290d06fc5edaefb5d054bbd72eda7f0d11f0c1d
mohammadsameerr <mohammadsameer@mahiratechnology>
2022-12-01 19:11:55 +0530

    added variable

---
+++ variable.tf
@@ -0,0 +1,12 @@
+#use of variable
+variable "ami_id" {
+    default = "ami-21f78e11"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "region" {
+    default = "ap-south-1a"
+}
---
+++ version.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1a"
+  acces_key = ""
+  secret_key = ""
+} 
+
+#Create Instance with docker container and use of variable.
+resource "aws_instance" "sam" {
+  ami               = var.ami_id
+  availability_zone = var.region
+  instance_type     = var.instance_type
+  vpc-security_group_ids = [
+    "  "
+  ]
+
+  tags = {
+    Name = "HelloSAm"
+  }
+
+#to create docker
+user_data = <<EOF
+#! /bin/sh
+yum update -y
+yum install docker -y
+service docker start
+docker pull nginx:alpine
+docker run -it --name my-container -p 80:80 nginx:alpine
+EOF
+}
+
+
+# create count meta argument instance.
+Vresource "aws_instance" "ec2" {
+  count = "var.instance_count"
+
+  ami           = "var.ami_id"
+  instance_type = "var.instance_type"
+
+  tags = {
+    Name = "ec2-${count.index}"
+  }
+}
